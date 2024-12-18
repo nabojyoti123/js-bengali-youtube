d9c70eae97a88c8121b0cb4f8ef06d6c8b74ae25
nabojyoti nath <107918155+nabojyoti123@users.noreply.github.com>
2024-12-18 01:45:18 +0000

    conversion of datatypes

---
+++ 01_basics/03_coversionoperations.js
@@ -0,0 +1,28 @@
+let score ="33abc";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber =Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" =>
+// "33abc" =>nan
+// true => 1; false=>0
+
+let isLoggedIn = "1";
+
+let booleanIsloggedIn =Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "NAbojyoti" => true
+
+let somenumber =33;
+let stringnumber = String(somenumber);
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+//......... operations .........
